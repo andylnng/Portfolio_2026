00721e0ac2b143b92d81042ee1e9aacddb4c0135
Andy Luong <luongandy98@gmail.com>
2026-03-15 17:38:13 -0400

    Restarting web design

--- src/views/StudyBuddy.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-8">
-    <h1 class="text-4xl font-bold text-white mb-4">Contact</h1>
-    <p class="text-gray-300">Get in touch with me.</p>
-  </div>
-</template>
-
-<script setup lang="ts">
-</script>
