9b08c1531ad808284ae4f2874ee2f9120edd3386
Andy Luong <luongandy98@gmail.com>
2026-03-15 21:48:41 -0400

    changed the names of my view and routes (might event remove this)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Resume from '../views/Resume.vue'
-import Yumzy from '../views/yumzy.vue'
-import StudyBuddy from '../views/StudyBuddy.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import About from "../views/About.vue";
+import Resume from "../views/Resume.vue";
+import Projects from "../views/Projects.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/about",
+      name: "about",
+      component: About,
     },
     {
-      path: '/resume',
-      name: 'resume',
-      component: Resume
+      path: "/resume",
+      name: "resume",
+      component: Resume,
     },
     {
-      path: '/yumzy',
-      name: 'yumzy',
-      component: Yumzy
+      path: "/projects",
+      name: "projects",
+      component: Projects,
     },
     {
-      path: '/studybuddy',
-      name: 'studybuddy',
-      component: StudyBuddy
+      path: "/conctact",
+      name: "contact",
+      component: Projects,
     },
-    {
-      path: '/stockly',
-      name: 'stockly',
-      component: StudyBuddy
-    }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
