0e3cf84b15fdc0b377bfa20c149abf3f074bcf8a
Andy Luong <luongandy98@gmail.com>
2026-03-15 21:48:06 -0400

    added new background image + about widget ios inspired

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="bg-[#222222] h-screen p-6 flex">
-    <SideNav />
-    <main class="flex-1 overflow-auto">
+  <div
+    class="relative min-h-screen bg-cover bg-center bg-no-repeat"
+    style="
+      background-image: url(macos-big-sur-apple-layers-fluidic-colorful-wwdc-stock-4096x2304-1455.jpg);
+    "
+  >
+    <div class="absolute inset-0" />
+    <main class="relative min-h-screen overflow-auto p-12 pb-36 md:pb-40">
       <RouterView />
+      <AboutWidget />
     </main>
+    <Dock />
   </div>
 </template>
 
 <script setup lang="ts">
-import SideNav from "./components/sidenav.vue";
+import Dock from "./components/Dock.vue";
+import AboutWidget from "./components/about-widget.vue";
 </script>
---
+++ src/components/about-widget.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="bg-white/10 backdrop-blur-3xl border border-white/10 p-6 rounded-2xl w-1/2 h-1/2"
+  >
+    <h2 class="text-3xl text-white mb-4">Hi, i'm Andy.</h2>
+    <div>
+      <p class="text-white mb-2">
+        I'm a passionate Front-end developer and UX/UI enthusiast with
+        experience in creating modern web applications. I enjoy solving complex
+        problems and building intuitive user experiences.
+      </p>
+      <p class="text-white">
+        I'm currently building
+        <label class="underline font-semibold">yumzy</label> a smart meal
+        planner that finds recipes based on your budget and what's available at
+        local grocery stores.
+      </p>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup></script>
